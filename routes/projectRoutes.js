c26b6d21ecb7de6dc3104d1b65cc851425399180
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-02-13 18:25:48 +0600

    routes added

---
+++ routes/projectRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+//router
+const router = express.Router();
+
+//GET all projects
+router.get("/", (req, res) => {
+  res.json({ message: "GET all projects" });
+});
+
+//GET a single project
+router.get("/:id", (req, res) => {
+  res.json({ message: "GET a single project" });
+});
+//GET a new project
+router.post("/", (req, res) => {
+  res.json({ message: "POST a  project" });
+});
+//DELETE a project
+router.delete("/:id", (req, res) => {
+  res.json({ message: "DELETE a  project" });
+});
+//UPDATE a project
+router.patch("/:id", (req, res) => {
+  res.json({ message: "PATCH a project" });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const projectRoutes = require("./routes/projectRoutes");
 
 //express app
 const app = express();
 
 //port
 const port = process.env.PORT || 4000;
+
+//middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/projects", projectRoutes);
 
 //listen for requests
 app.listen(port, () => {
