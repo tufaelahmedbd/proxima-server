a01e486ee998cba63c8ef1843a3463d1e552938d
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-03-14 04:56:47 +0600

    patch empty_fields added

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -26,20 +26,41 @@ const getSingleProject = async (req, res) => {
 
 //post a new project
 const postProject = async (req, res) => {
-  const data = req.body;
-
+  const { title, tech, budget, duration, manager, dev } = req.body;
+  let emptyFields = [];
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!tech) {
+    emptyFields.push("tech");
+  }
+  if (!budget) {
+    emptyFields.push("budget");
+  }
+  if (!duration) {
+    emptyFields.push("duration");
+  }
+  if (!manager) {
+    emptyFields.push("manager");
+  }
+  if (!dev) {
+    emptyFields.push("dev");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill-up all fields", emptyFields });
+  }
   try {
     //saved as project on db
     const project = await Project.create({
-      ...data,
+      ...req.body,
     });
 
     res.status(200).json(project);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
-
-  res.json({ message: "POST a  project" });
 };
 
 //delete a project
@@ -62,12 +83,41 @@ const deleteProject = async (req, res) => {
 
 const updateProject = async (req, res) => {
   const { id } = req.params;
+  const { title, tech, budget, duration, manager, dev } = req.body;
+  let emptyFields = [];
+  if (!title) {
+    emptyFields.push("title");
+  }
+  if (!tech) {
+    emptyFields.push("tech");
+  }
+  if (!budget) {
+    emptyFields.push("budget");
+  }
+  if (!duration) {
+    emptyFields.push("duration");
+  }
+  if (!manager) {
+    emptyFields.push("manager");
+  }
+  if (!dev) {
+    emptyFields.push("dev");
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill-up all fields", emptyFields });
+  }
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: "Invalid id" });
   }
 
-  const project = await Project.findOneAndUpdate({ _id: id }, { ...req.body });
+  const project = await Project.findOneAndUpdate(
+    { _id: id },
+    { ...req.body },
+    { new: true }
+  );
 
   if (!project) {
     return res.status(400).json({ error: "No project found" });
