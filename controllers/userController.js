cd33056087c5404b5ab9d8810dc5cc4548479dde
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-03-21 03:22:37 +0600

    login & signup routes added

---
+++ controllers/userController.js
@@ -0,0 +1,14 @@
+//login user
+const loginUser = async (req, res) => {
+  res.json({ message: "login" });
+};
+
+//signup user
+const signupUser = async (req, res) => {
+  res.json({ message: "signup" });
+};
+
+module.exports = {
+  loginUser,
+  signupUser,
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const projectRoutes = require("./routes/projectRoute");
+const userRoutes = require("./routes/userRoute");
 
 //express app
 const app = express();
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/projects", projectRoutes);
+app.use("/api/user", userRoutes);
 
 //mongodb
 mongoose.set("strictQuery", false);
